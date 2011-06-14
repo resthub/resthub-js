34fc5792821cb040e1a059e0ab66b7c36b202d84
Nicolas Carlier <nicolas.carlier@atosorigin.com>
2011-06-14 14:46:46 +0200

    Reintroduce i18n module and add test cases

---
+++ src/test/i18n/nls/fr/labels.js
@@ -0,0 +1,5 @@
+define({
+	'hello': {
+		'world': 'Bonjour le monde!'
+	}	
+});
---
+++ src/test/i18n/nls/labels.js
@@ -0,0 +1,10 @@
+define({
+	// 'root' is mandatory (define default locale).
+	'root': {
+		'hello': {
+			'world': 'Hello World!'
+		}
+	},
+	// Define French locale.
+	"fr": true
+});
