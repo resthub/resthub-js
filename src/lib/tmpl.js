6afe050e4981015492daa157a1f05265e9fb5b34
Nicolas Carlier <nicolas.carlier@atosorigin.com>
2011-06-30 10:28:44 +0200

    Add support of IE<9 with JQuery TMPL

--- src/lib/tmpl.js
+++ src/lib/tmpl.js
@@ -1,4 +1,4 @@
-define(['lib/jquery', 'lib/jquery/jquery.tmpl'], function () {
+define(['lib/jquery', 'lib/jquery/jquery.tmpl', 'lib/innershiv'], function () {
 
 	$.fn.render = function( url, data, options, parentItem ) {
 		var element = this[0];
@@ -8,7 +8,13 @@ define(['lib/jquery', 'lib/jquery/jquery.tmpl'], function () {
 			url: url,
 			async: false,
 			success: function(template) {
-				$.tmpl(template, data, options, parentItem).appendTo(element);
+				var tmpl = null;
+				if (typeof window.Modernizr != 'undefined') {
+					tmpl = $.tmpl('<div>' + template + '</div>', data, options, parentItem);
+					$(element).append(innerShiv(tmpl, true));
+				} else {
+					$.tmpl(template, data, options, parentItem).appendTo(element);
+				}
 			}
 		});
 	};
---
+++ src/test/ie/app.js
@@ -0,0 +1,71 @@
+define(['lib/resthub'], function() {
+
+    // A template string
+    var tmpl = '<section><a href="${url}">${lastName}</a></section>';
+
+    $('.localSimpleTemplate').click(function() {
+            var dataObject = {
+                    firstName: "John",
+                    lastName: "Resig",
+                    url: "http://ejohn.org/",
+                    cities: [
+                            "Boston, MA",
+                            "San Francisco, CA"
+                    ]
+            };
+
+            $('.content').empty();
+            $.tmpl( tmpl, dataObject ).appendTo( '#main .content' );
+    });
+
+    $('.localEachTemplate').click(function() {
+            var people = [
+                    {
+                            firstName: "John",
+                            lastName: "Resig",
+                            url: "http://ejohn.org/",
+                            cities: [
+                                    { name: "Boston", state: "MA" },
+                                    { name: "San Francisco", state: "CA" }
+                            ]
+                    },
+                    {
+                            firstName: "Dave",
+                            lastName: "Reed",
+                            url: "http://dave.org/",
+                            cities: [
+                                    { name: "Seattle", state: "WA" },
+                                    { name: "Los Angeles", state: "CA" },
+                                    { name: "New York", state: "NY" }
+                            ]
+                    },
+                    {
+                            firstName: "Boris",
+                            lastName: "Moore",
+                            url: "http://boris.org/",
+                            cities: [
+                                    { name: "Redmond", state: "WA" }
+                            ]
+                    }
+            ];
+
+            $('.content').empty();
+            $.tmpl( tmpl, people ).appendTo( '#main section.content' );
+
+    });
+
+    $('.remoteSimpleTemplate').click(function() {
+            var dataObject = {
+                    firstName: "John",
+                    lastName: "Resig",
+                    url: "http://ejohn.org/",
+                    cities: [
+                            "Boston, MA",
+                            "San Francisco, CA"
+                    ]
+            };
+
+            $('#main > section.content').render('./simpletemplate.html', dataObject);
+    });
+		
+});
