f52b1834f4c4faacc1bd8fc82e2346df9071c5ff
Mickael Daniel <daniel.mickael@gmail.com>
2011-07-05 15:45:58 +0200

    tests: json

---
+++ src/test/json/json.js
@@ -0,0 +1,18 @@
+// ## Basic JSON Testsuite
+
+require({baseUrl: "../../"}, ['lib/json'], function() {
+
+	module('json');
+
+	test('basic JSON.stringify test', function() {
+		var o = {plugin: 'jquery-json', version: 2.2};
+		equals($.toJSON(o), JSON.stringify(o), 'same as calling JSON.stringify...');
+	});
+	
+	test('basic JSON.parse test', function() {
+		var o = $.toJSON({plugin: 'jquery-json', version: 2.2});
+		deepEqual($.evalJSON(o), JSON.parse(o), 'same as calling JSON.parse...');
+	});
+
+
+});
