45796b40d0ae726367b2a62b869d5171b28c798e
Mickael Daniel <daniel.mickael@gmail.com>
2011-07-05 16:33:24 +0200

    tests: ie

---
+++ src/test/ie/ie.js
@@ -0,0 +1,43 @@
+/**
+ * ## Basic ie support test suite
+ */
+require({baseUrl: "../../"}, ['lib/resthub'], function() {
+
+    var tmpl = '<li><a href="${url}">${name}</a></li>',
+    data = [{
+      name: "Resig",
+      url: "http://ejohn.org/"     
+    }, {
+        name: 'Reed',
+        url: 'http://weblogs.asp.net/infinitiesloop/',
+    }, {
+        name: 'Moore',
+        url: "http://www.borismoore.com",
+    }];
+
+    module('tmpl');
+
+    test('should be ok with basic templating', function() {
+        equals($.tmpl( tmpl, data[0] ).html(), '<a href="http://ejohn.org/">Resig</a>', 'expect basic anchor markup');
+        equals($.tmpl( tmpl, data ).text(), 'ResigReedMoore', 'expect enumerable to acts accordingly');
+
+        // $.fn.render do not return itself, no chaining sugar...
+        // also it works synchronously...
+        var el = $('ul');
+
+        el.render('tmpl.fixture.html', data);
+
+        equals($.trim(el.text()), 'Remote:Resig Remote:Reed Remote:Moore', 'using remote should be ok');
+    });
+    
+    test('should be ok with ie8 related testing', function() {
+        var tpl = "<strong>${hero}</strong>",
+        model = [{hero: "Chuck Norris"}, {hero: "Steven Seagal"}];
+       
+        result = $.tmpl( tpl, model ).text();
+        
+        equals(result, 'Chuck NorrisSteven Seagal', 'Chuck Norris does not use templates, templates use Chuck Norris');
+
+    });
+
+});
