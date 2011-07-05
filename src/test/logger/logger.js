7b34f304c8a1a471b4493529557f6a55340432a9
Mickael Daniel <daniel.mickael@gmail.com>
2011-07-05 15:26:29 +0200

    tests: logger (todo: guess how to programatically check the console outputs)

--- src/test/logger/logger.js
+++ src/test/logger/logger.js
@@ -1,51 +1,44 @@
-define(['lib/console'], function() {
-	
-	$('#failedDebug').click(function(evt){
-		evt.preventDefault();
-		console.debug('It failed : '+toto.name);
-		return false;
-	});
-	
-	$('#debug').click(function(evt){
-		evt.preventDefault();
-		console.debug('It works');
-		return false;
-	});
-	
-	$('#filteredError').click(function(evt){
-		evt.preventDefault();
+// ## Basic Console Testsuite
+
+// TODO: Guess how to programatically check the console output (cross-browserly, 
+require({baseUrl: "../../"}, ['lib/console'], function() {
+
+	module('console');
+
+	test('should feature basic filterered ouput', function() {
 		LOGGER_CONF['logger.unknown'] = 'unknown';
-		
+
 		// Unknown level must appear.
 		console.debug('[logger.unknown] Must appear');
-		
+
 		// Unconfigured logger name must appear.
 		console.debug('[unknown] Must appear');
-		
+
 		// No logger must appear.
 		console.debug('Must appear');
-		
+
 		// Misformated logger must appear.
 		console.debug('[mistformated.logger MUST appear');
 
-		return false;
+		// todo: fix this to be actual tests
+		ok(true, 'blind test');
+
 	});
-	
-	$('#filteredDebug').click(function(evt){
-		evt.preventDefault();
+
+	test('should feature basic filterered ouput with debug', function() {
 		LOGGER_CONF['logger.debug'] = 'debug';
 		LOGGER_CONF['logger.info'] = 'info';
 		LOGGER_CONF['logger.warn'] = 'warn';
 		LOGGER_CONF['logger.error'] = 'error';
 		LOGGER_CONF['logger.off'] = 'off';
 		LOGGER_CONF['logger.all'] = 'all';
-		
+
 		// Filtering on debug level
 		console.debug('[logger.debug] Must appear');
 		console.info('[logger.debug] Must appear');
 		console.warn('[logger.debug] Must appear');
 		console.error('[logger.debug] Must appear');
-		
+
 		// Filtering on info level
 		console.debug('[logger.info] Must NOT appear');
 		console.info('[logger.info] Must appear');
@@ -75,13 +68,11 @@ define(['lib/console'], function() {
 		console.info('[logger.off] Must NOT appear');
 		console.warn('[logger.off] Must NOT appear');
 		console.error('[logger.off] Must NOT appear');
-		return false;
+		
+		ok(true, 'blind test');
+
 	});
 	
-	$('#dir').click(function(evt){
-		var name = "toto";
-		evt.preventDefault();
-		console.debug({message:'It works'});
-		return false;
-	});
+	
+
 });
