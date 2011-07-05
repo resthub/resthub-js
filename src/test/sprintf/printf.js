b797372d63a1a1b3e59d510dc23d24ac3656b3c0
Mickael Daniel <daniel.mickael@gmail.com>
2011-07-05 09:56:27 +0200

    tests: sprintf

---
+++ src/test/sprintf/printf.js
@@ -0,0 +1,15 @@
+/**
+ * ## Basic Console Testsuite
+ */
+require({baseUrl: "../../"}, ['lib/jquery/jquery.sprintf'], function() {
+
+    module('printf');
+    
+
+    test('basic printf should be ok', function() {
+        equals($.sprintf('Hello %s %s !', 'excited', 'world'), 'Hello excited world !');
+        equals($.sprintf('Hello %s %s !', 'excited', 'world'), 'Hello excited world !');
+    });
+        
+});
+
