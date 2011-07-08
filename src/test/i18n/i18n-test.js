afb61c8211f8acef7b74414c2370c8f5756241d5
Mickael Daniel <daniel.mickael@gmail.com>
2011-07-08 10:39:17 +0200

    tests ie: log remove

--- src/test/i18n/i18n-test.js
+++ src/test/i18n/i18n-test.js
@@ -3,11 +3,9 @@
  */
 require({baseUrl: "../../"}, ['i18n!test/i18n/nls/labels'], function(i18n) {
 
-	console.log(arguments);
 	module('i18n');
 
-	test('should i18n works properly', 1, function() {
-		console.log(i18n);
+	test('should i18n works properly', 1, function() {		
 		equals(i18n.hello.world, 'Bonjour le monde!', '...')
 	});
 	
